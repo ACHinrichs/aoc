3070d500e63f2f007492e1a80eeb48b95d38cc43
Adrian C. Hinrichs <adrian.hinrichs@rwth-aachen.de>
2021-12-01 06:32:29 +0100

    Add 01 part 1

--- 01-increase-count/src/main.rs
+++ 01-increase-count/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("input.txt").expect("file not found");
+    let reader = BufReader::new(file);
+    let mut last = -1;
+    let mut inc_count = -1; //initialize in a way that the first line does not count
+    
+    for line in reader.lines(){
+	let current = line.unwrap().parse().unwrap_or(-1);
+	if current > last {
+	    inc_count = inc_count + 1;
+	}
+	last = current;
+    }
+    println!("{}", inc_count);
 }
