100ad28bf58caba41f871b8185b32eb2e732f5e3
Adrian C. Hinrichs <adrian.hinrichs@rwth-aachen.de>
2021-12-01 06:40:47 +0100

    Refactor

--- 01-increase-count/src/main.rs
+++ 01-increase-count/src/main.rs
@@ -5,15 +5,24 @@ use std::io::BufRead;
 fn main() {
     let file = File::open("input.txt").expect("file not found");
     let reader = BufReader::new(file);
+    let inc_count = count_inc(reader.lines()
+			      .map(|x| x.unwrap().parse().unwrap_or(-1)).collect()); //initialize in a way that the first line does not count
+    
+    println!("{}", inc_count);
+}
+
+
+fn count_inc(lines: Vec<i32>) -> i32 {
     let mut last = -1;
     let mut inc_count = -1; //initialize in a way that the first line does not count
     
-    for line in reader.lines(){
-	let current = line.unwrap().parse().unwrap_or(-1);
+    for line in lines{
+	let current = line;
 	if current > last {
 	    inc_count = inc_count + 1;
 	}
 	last = current;
     }
-    println!("{}", inc_count);
-}
+    
+    inc_count
+} 
