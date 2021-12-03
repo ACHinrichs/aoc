901b17e5d08bf31d1ff4c81e9d966935f1d81a11
Adrian C. Hinrichs <adrian.hinrichs@rwth-aachen.de>
2021-12-03 09:12:23 +0100

    Finally add part 2 of binary diagnostic

--- 03-binary_diagnostic/src/main.rs
+++ 03-binary_diagnostic/src/main.rs
@@ -29,14 +29,70 @@ fn main() {
 	    print!("0");
 	}
     }
+
+    let mut oxyrating = bits_vec.iter().map(|x| x).collect::<Vec<&Vec<u32>>>(); //wtf
+    let mut pos = 0;
+
+    //println!("{}", pos);
+    while oxyrating.len() > 1 && pos < *len {
+	println!("{} {}", pos, oxyrating.len());
+	println!("{:?}", oxyrating);
+	let ones = count_ones(&oxyrating.iter().map(|x| x[pos]).collect());
+	let zeros = oxyrating.len() as u32 - ones;
 	
+	let most_common;  
+	
+	if ones >= zeros{
+	    most_common = 1;
+	    println!("1");
+	}else{
+	    most_common = 0;
+	    println!("0");
+	}
+	
+	oxyrating = (oxyrating.iter()).filter(|x| x[pos] == most_common)
+	    .map(|x| *x)
+	    .collect::<Vec<&Vec<u32>>>();
+
+	pos += 1;
+    }
+    println!("{} {}", pos, oxyrating.len());
+    println!("\noxyrating: {:?}", oxyrating);
+	
+    let mut co2rating = bits_vec.iter().map(|x| x).collect::<Vec<&Vec<u32>>>(); //wtf
+    pos = 0;
+    while co2rating.len() > 1 && pos < *len {
+	let ones = count_ones(&co2rating.iter().map(|x| x[pos]).collect());
+	let zeros = co2rating.len() as u32 - ones;
+	
+	let mut least_common = 2;  
+	
+	if ones >= zeros{
+	    least_common = 0;
+	}else if zeros > ones{
+	    least_common = 1;
+	}
+	
+	co2rating = (co2rating.iter()).filter(|x| x[pos] == least_common)
+	    .map(|x| *x)
+	    .collect::<Vec<&Vec<u32>>>();
+
+	pos += 1;
+    }
+    
     print!("\n");
 
     println!("Results:\n");
     println!("\ngamma: {}", gamma);
     println!("\nepsilon: {}", epsilon);
 
-    println!("gamma * epsilon = {}", gamma * epsilon)
+    
+    println!("gamma * epsilon = {}", gamma * epsilon);
+
+    println!("\noxyrating: {:?}, {}", oxyrating, bin_to_dec(oxyrating[0]));
+    println!("co2rating: {:?}, {}", co2rating, bin_to_dec(co2rating[0]));
+    println!("combined: {}", bin_to_dec(oxyrating[0]) * bin_to_dec(co2rating[0]));
+
 }
 
 fn count_ones(bits: &Vec<u32>) -> u32{
@@ -47,4 +103,9 @@ fn count_ones(bits: &Vec<u32>) -> u32{
 	}
     }
     res
+}
+
+
+fn bin_to_dec(bits: &Vec<u32>) -> u32{
+    bits.iter().fold(0,|x,y| x*2+*y)
 }
