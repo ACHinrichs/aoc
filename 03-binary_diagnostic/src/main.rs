f1cbc9c84a4acc7f8ec3b6cd968389a48ce09ded
Adrian C. Hinrichs <adrian.hinrichs@rwth-aachen.de>
2021-12-03 08:14:01 +0100

    Refactor

--- 03-binary_diagnostic/src/main.rs
+++ 03-binary_diagnostic/src/main.rs
@@ -7,31 +7,21 @@ fn main() {
     let lines = &BufReader::new(file).lines().map(|x| x.expect("ERR: while reading the File!")).map(String::from).collect::<Vec<String>>();
 
     let len = &lines[0].len();
-    let mut ones = vec![0; *len];
-    let mut zeros = vec![0; *len];
 
-    let mut linecount = 0;
+    let mut bits_vec: Vec<Vec<u32>> = Vec::new();
     for l in lines{
-	linecount += 1;
-	for i in 0..*len{
-	    let c =  l.chars().nth(i).expect("ERR: String to CHars failed");
-	    //println!("{:?}",c);
-	    match c {
-		'1' => ones[i]+= 1,
-		'0' => zeros[i]+= 1,
-		_ => println!("unexpected value {}", c)
-	    }
-	}
-	print!(".");
+	let bits = l.chars().map(|x| x as u32 -48).collect();// '0' is 48 in ascii
+	bits_vec.push(bits);
     }
-    println!("{} Lines processed", linecount);
-
     let mut gamma = 0;
-    let mut epsilon = 0; 
+    let mut epsilon = 0;
+	
     for i in 0..*len{
+	let ones = count_ones(&bits_vec.iter().map(|x| x[i]).collect());
+	let zeros = bits_vec.len() as u32 - ones;
 	gamma = gamma * 2;
 	epsilon = epsilon * 2;
-	if ones[i] > zeros[i]{
+	if ones > zeros{
 	    gamma += 1;
 	    print!("1");
 	}else{
@@ -39,13 +29,22 @@ fn main() {
 	    print!("0");
 	}
     }
+	
     print!("\n");
 
     println!("Results:\n");
-    println!("ones: {:?}", ones);
-    println!("zeros: {:?}", zeros);
     println!("\ngamma: {}", gamma);
     println!("\nepsilon: {}", epsilon);
 
     println!("gamma * epsilon = {}", gamma * epsilon)
+}
+
+fn count_ones(bits: &Vec<u32>) -> u32{
+    let mut res = 0;
+    for i in bits{
+	if *i == 1 {
+	    res += 1;
+	}
+    }
+    res
 }
