637b72e3dc881885f89c2c538e6e20cc63a80a7e
Adrian C. Hinrichs <adrian.hinrichs@rwth-aachen.de>
2021-12-04 14:34:35 +0100

    Add part2 and refactor a little

--- 04-giant-squid/src/main.rs
+++ 04-giant-squid/src/main.rs
@@ -34,33 +34,52 @@ fn read_file(filename: &str) -> (Vec<i32>, Vec<Vec<Vec<i32>>>){
 }
 
 fn main() {
-    let (numbers, mut boards) = read_file("input.txt");
+    let (numbers, boards) = read_file("input.txt");
     println!("Got {} boards and {} numbers", boards.len(), numbers.len());
 
-    let mut finished = false;
-    let mut winning_board = boards.len();
+    part2(numbers, boards);
+}
+
+fn part2(numbers: Vec<i32>, mut boards: Vec<Vec<Vec<i32>>>){
+    let mut finished_order: Vec<(Vec<Vec<i32>>, i32)> = Vec::new();
     let mut number_index = 0;
     
-    while !finished && number_index < numbers.len()  {
+    while number_index < numbers.len() && boards.len() > 0 {
 	mark_number(&mut boards, numbers[number_index]);
 
-	let res = check_for_winning_board(&boards);
-	if res.is_ok(){
-	    winning_board = res.unwrap();
-	    finished = true;
-	} else {
-	    number_index += 1;
+	let mut new_boards: Vec<Vec<Vec<i32>>> = Vec::new();
+	for i in 0..boards.len(){
+	    if board_finished(&boards[i]){
+		finished_order.push((copy_board(&boards, i), numbers[number_index]));
+	    } else {
+		new_boards.push(copy_board(&boards, i));
+	    }
 	}
+	boards = new_boards;
+
+	number_index += 1;
     }
-
-    println!("Winning board has index {:?}", winning_board);
-
+    let winning_board = &finished_order[0].0;
+    println!("Winning board is {:?}", winning_board);
     // Collect all positive numbers of winning board
+    let winning_sum = collect_positive(winning_board);
+    println!("Winning-Sum is {:?}", winning_sum);
+    println!("Winning-Sum * last number is {}", winning_sum * finished_order[0].1);
 
-    let sum = boards[winning_board].iter().map(|y| y.iter().fold(0, |b, x| b + cmp::max(*x,0))).fold(0, |b, x| b + cmp::max(x,0));
-    println!("Sum is {}", sum);
-    println!("Sum * last number is {}", sum * numbers[number_index]);
+    let loosing_sum = collect_positive(&finished_order[finished_order.len()-1].0);
+    println!("Loosing-Sum is {:?}", loosing_sum);
+    println!("Loosing-Sum * last number is {}", loosing_sum * finished_order[finished_order.len()-1].1);
 }
+
+fn collect_positive(board: &Vec<Vec<i32>>) -> i32{
+    (*board).iter().map(|y| y.iter().fold(0, |b, x| b + cmp::max(x,&0)))
+	.fold(0, |b, x| b + cmp::max(x,0))
+}
+
+fn copy_board(boards: &Vec<Vec<Vec<i32>>>, i: usize) -> Vec<Vec<i32>>{
+    boards[i].iter().map(|x| x.iter().map(|x| *x).collect::<Vec<i32>>()).collect::<Vec<Vec<i32>>>()
+}
+
 
 fn mark_number(boards: &mut Vec<Vec<Vec<i32>>>, number: i32){
     for i in 0..boards.len(){
@@ -72,15 +91,6 @@ fn mark_number(boards: &mut Vec<Vec<Vec<i32>>>, number: i32){
 	    }
 	}
     }
-}
-    
-fn check_for_winning_board(boards: &Vec<Vec<Vec<i32>>>) -> Result<usize, bool>{
-    for i in 0..boards.len(){
-	if board_finished(&boards[i]){
-	    return Ok(i);
-	}
-    }
-    Err(false)
 }
 
 fn board_finished(board: &Vec<Vec<i32>>) -> bool{
