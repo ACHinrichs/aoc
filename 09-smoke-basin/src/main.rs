1932dfc015145c84e2db0e3aded25e220a56a596
Adrian C. Hinrichs <adrian.hinrichs@rwth-aachen.de>
2021-12-09 06:40:16 +0100

    Implement 09 p1

--- 09-smoke-basin/src/main.rs
+++ 09-smoke-basin/src/main.rs
@@ -1,3 +1,31 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("input.txt").expect("file not found");
+    let ground = BufReader::new(file)
+        .lines()
+        .map(|x| {
+            x.unwrap()
+                .chars()
+                .map(|y| y.to_digit(10).unwrap())
+                .collect()
+        })
+        .collect::<Vec<Vec<u32>>>();
+    println!("{:?}", ground);
+
+    let mut low_level_sum = 0;
+    for i in 0..ground.len() {
+        for j in 0..ground[i].len() {
+            if (i <= 0 || ground[i][j] < ground[i - 1][j])
+                && (i >= ground.len() - 1 || ground[i][j] < ground[i + 1][j])
+                && (j <= 0 || ground[i][j] < ground[i][j - 1])
+                && (j >= ground[i].len() - 1 || ground[i][j] < ground[i][j + 1])
+            {
+                low_level_sum += ground[i][j] + 1;
+            }
+        }
+    }
+    println!("Low Level Sum (Part 1) is {}", low_level_sum);
 }
