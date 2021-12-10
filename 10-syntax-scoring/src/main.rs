b5159125a3837273789ebd97309c72cdb5316a45
Adrian C. Hinrichs <adrian.hinrichs@rwth-aachen.de>
2021-12-10 08:58:38 +0100

    Add p2

--- 10-syntax-scoring/src/main.rs
+++ 10-syntax-scoring/src/main.rs
@@ -10,14 +10,20 @@ fn main() {
         .collect::<Vec<Vec<char>>>();
 
     let mut part_1_score = 0;
-    let mut part_2_score = 0;
+    let mut part_2_scores: Vec<u64> = Vec::new();
     for l in lines {
         let (syntaxcheck_points, completion_points) = parse(l);
         part_1_score += syntaxcheck_points;
-        part_2_scroe += completion_points;
+        if completion_points > 0 {
+            part_2_scores.push(completion_points);
+        }
     }
     println!("Score for Part 1 is {}", part_1_score);
-    println!("Score for Part 2 is {}", part_2_score);
+    part_2_scores.sort();
+    println!(
+        "Score for Part 2 is {}",
+        part_2_scores[part_2_scores.len() / 2]
+    );
 }
 
 // Returns (x,y), x are the points for syntax checking (p1), so 0 if correct,
@@ -37,7 +43,7 @@ fn parse(line: Vec<char>) -> (u64, u64) {
                     let last = stack.pop().expect("There has to be smthng on the stack");
                     if !matching(last, *cur) {
                         // Zeile genau so korrupt wie die CDU
-                        println!("Cant close a {} with {}", last, cur);
+                        //println!("Cant close a {} with {}", last, cur);
                         return (syntax_checker_points(*cur), 0);
                     }
                 }
@@ -45,7 +51,23 @@ fn parse(line: Vec<char>) -> (u64, u64) {
             _ => continue,
         }
     }
-    (false, 0)
+    // Stack is empty iff line is correct, otherwise there are only opening brackets on the stack
+    let mut points = 0;
+    while !stack.is_empty() {
+        let s = stack.pop().expect("There has to be smthng on the stack");
+        points = points * 5 + autocompletion_points(s);
+    }
+    (0, points)
+}
+
+fn autocompletion_points(s: char) -> u64 {
+    match s {
+        '(' => 1,
+        '[' => 2,
+        '{' => 3,
+        '<' => 4,
+        _ => u64::MAX,
+    }
 }
 
 fn syntax_checker_points(s: char) -> u64 {
