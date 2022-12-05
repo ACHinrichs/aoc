285fbf1f316b8848dc0bcd45c089eb39975a74f4
Adrian Hinrichs <adrian.hinrichs@rwth-aachen.de>
2022-12-05 06:14:54 +0100

    Add strucutre for day 5

---
+++ 2022/05-supply-stacks/src/main.rs
@@ -0,0 +1,30 @@
+use std::env;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn main() {
+    println!("AOC 2022, Day 05 – Supply-Stacks\n");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        panic!("Please supply the task and the problem-input to solve as arguments!");
+    }
+    let task = &args[1];
+
+    let file = File::open(&args[2]).expect(&format!("Inputfile {} not found", args[1]));
+    let lines = &mut BufReader::new(file)
+        .lines()
+        .map(|x| x.unwrap().to_string())
+        .retain(|l| !l.starts_with("//"))
+        .collect::<Vec<String>>();
+
+    if task == "1" {
+    } else if task == "2" {
+    } else {
+        panic!("Task unknown, please specify as first argument")
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
