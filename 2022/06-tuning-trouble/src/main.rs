74e9586b6cb202e1035da285d0b966a4a413ca6e
Adrian Hinrichs <adrian.hinrichs@rwth-aachen.de>
2022-12-06 06:16:06 +0100

    Add solution for p1

--- 2022/06-tuning-trouble/src/main.rs
+++ 2022/06-tuning-trouble/src/main.rs
@@ -18,9 +18,23 @@ fn main() {
         .filter(|l| !l.starts_with("//"))
         .collect::<Vec<String>>();
 
-    if task == "1" {
-    } else if task == "2" {
-    } else {
-        panic!("Task unknown, please specify as first argument")
+    for l in lines {
+        //Just so that I can test all examples at once
+        if task == "1" {
+            for i in 3..l.len() {
+                let cur = &mut l.chars().collect::<Vec<char>>()[i - 3..i + 1];
+                if !(1..cur.len()).any(|i| cur[i..].contains(&cur[i - 1])) {
+                    //Check for douplicate
+                    println!(
+                        "The first index to end an 4-letter non duplicate block is {}",
+                        i + 1
+                    );
+                    break;
+                }
+            }
+        } else if task == "2" {
+        } else {
+            panic!("Task unknown, please specify as first argument")
+        }
     }
 }
