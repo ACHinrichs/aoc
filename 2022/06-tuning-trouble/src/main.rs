a8338b3183255525ecf7e6610d2f1182c6f9b663
Adrian Hinrichs <adrian.hinrichs@rwth-aachen.de>
2022-12-06 06:19:41 +0100

    remove code duplication

--- 2022/06-tuning-trouble/src/main.rs
+++ 2022/06-tuning-trouble/src/main.rs
@@ -20,32 +20,25 @@ fn main() {
 
     for l in lines {
         //Just so that I can test all examples at once
+        let size;
         if task == "1" {
-            for i in 3..l.len() {
-                let cur = &mut l.chars().collect::<Vec<char>>()[i - 3..i + 1];
-                if !(1..cur.len()).any(|i| cur[i..].contains(&cur[i - 1])) {
-                    //Check for douplicate
-                    println!(
-                        "The first index to end an 4-letter non duplicate block is {}",
-                        i + 1
-                    );
-                    break;
-                }
-            }
+            size = 4;
         } else if task == "2" {
-            for i in 13..l.len() {
-                let cur = &mut l.chars().collect::<Vec<char>>()[i - 13..i + 1];
-                if !(1..cur.len()).any(|i| cur[i..].contains(&cur[i - 1])) {
-                    //Check for douplicate
-                    println!(
-                        "The first index to end a 14-letter non duplicate block is {}",
-                        i + 1
-                    );
-                    break;
-                }
-            }
+            size = 14;
         } else {
             panic!("Task unknown, please specify as first argument")
+        }
+        for i in (size - 1)..l.len() {
+            let cur = &mut l.chars().collect::<Vec<char>>()[i - (size - 1)..i + 1];
+            if !(1..cur.len()).any(|i| cur[i..].contains(&cur[i - 1])) {
+                //Check for douplicate
+                println!(
+                    "The first index to end an {}-letter non duplicate block is {}",
+                    size,
+                    i + 1
+                );
+                break;
+            }
         }
     }
 }
