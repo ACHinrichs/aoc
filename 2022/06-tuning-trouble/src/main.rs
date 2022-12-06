3cae68cf48624b4c0444cfd46e04e8789ef2c4b4
Adrian Hinrichs <adrian.hinrichs@rwth-aachen.de>
2022-12-06 06:01:57 +0100

    Add skeleton for 06

---
+++ 2022/06-tuning-trouble/src/main.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn main() {
+    println!("AOC 2022, Day 06 – Tuning-Trouble\n");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        panic!("Please supply the task and the problem-input to solve as arguments!");
+    }
+    let task = &args[1];
+
+    let file = File::open(&args[2]).expect(&format!("Inputfile {} not found", args[1]));
+    let lines = &mut BufReader::new(file)
+        .lines()
+        .map(|x| x.unwrap().to_string())
+        .filter(|l| !l.starts_with("//"))
+        .collect::<Vec<String>>();
+
+    if task == "1" {
+    } else if task == "2" {
+    } else {
+        panic!("Task unknown, please specify as first argument")
+    }
+}
