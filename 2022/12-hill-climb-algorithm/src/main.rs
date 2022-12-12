ee6233df9e9c12bd03cb2f2ab0c01ccc41d9da25
Adrian Hinrichs <adrian.hinrichs@rwth-aachen.de>
2022-12-12 11:19:31 +0100

    WIP Add solution for d12 p 1
    Some error still existent, verry slow, oom-killer killed my WM on
    the real data

---
+++ 2022/12-hill-climb-algorithm/src/main.rs
@@ -0,0 +1,145 @@
+use colored::*;
+use std::collections::VecDeque;
+use std::env;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+fn main() {
+    println!("AOC 2022, Day 12 – Hill Climb Algorithm\n");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        panic!("Please supply the task and the problem-input to solve as arguments!");
+    }
+    let task = &args[1];
+
+    let file = File::open(&args[2]).expect(&format!("Inputfile {} not found", args[1]));
+    let lines = BufReader::new(file)
+        .lines()
+        .map(|x| x.unwrap().to_string())
+        .filter(|l| !l.starts_with("//"))
+        .collect::<Vec<String>>();
+    let mut heightmap: Vec<Vec<u32>> = Vec::new();
+
+    //Parse trees and save start and end location
+    let mut start_x = 0;
+    let mut start_y = 0;
+    let mut end_x = 0;
+    let mut end_y = 0;
+
+    let mut y = 0;
+    let mut x = 0;
+    for l in lines {
+        let mut tmp_line = Vec::new();
+        for c in l.chars() {
+            let mut value = c as u32;
+            if c == 'S' {
+                value = 'a' as u32;
+                start_x = x + 1; // Add 1 because we will add a border later!
+                start_y = y + 1; // Add 1 because we will add a border later!
+            } else if c == 'E' {
+                value = 'z' as u32;
+                end_x = x + 1; // Add 1 because we will add a border later!
+                end_y = y + 1; // Add 1 because we will add a border later!
+            }
+            value = value - 'a' as u32 + 1;
+            tmp_line.push(value);
+            x += 1;
+        }
+        heightmap.push(tmp_line);
+        y += 1;
+        x = 0;
+    }
+
+    // Add border of height z+2, so that it can not be reached, therefore we need not to worry about over and underflows of the array index
+    let len_lines_without_border = heightmap[0].len();
+    let unreachable_height = 42;
+    heightmap.insert(0, vec![unreachable_height; len_lines_without_border]);
+    heightmap.push(vec![unreachable_height; len_lines_without_border]);
+    heightmap = heightmap
+        .into_iter()
+        .map(|mut l| {
+            l.insert(0, unreachable_height);
+            l
+        })
+        .map(|mut l| {
+            l.push(unreachable_height);
+            l
+        })
+        .collect();
+
+    let mut visited = Vec::new();
+    for i in 0..heightmap.len() {
+        visited.push(vec![false; heightmap[i].len()]);
+    }
+    dbg!(&heightmap);
+    if task == "1" {
+        let (found, length) = bfs(start_x, start_y, end_x, end_y, &heightmap, &mut visited);
+        if found {
+            println!("Found a path of size {}!", length);
+        } else {
+            println!("Did not find a path");
+        }
+    } else if task == "2" {
+    } else {
+        panic!("Task unknown, please specify as first argument")
+    }
+}
+
+fn bfs(
+    start_x: usize,
+    start_y: usize,
+    end_x: usize,
+    end_y: usize,
+    heights: &Vec<Vec<u32>>,
+    visited: &mut Vec<Vec<bool>>,
+) -> (bool, u32) {
+    let directions = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+    let mut queue = VecDeque::new();
+    queue.push_back((start_x, start_y, 0));
+    while queue.len() > 0 {
+        let (cur_x, cur_y, cur_length) = queue.pop_front().unwrap();
+        //dbg!(cur_x, cur_y, end_x, end_y);
+        print_maps(cur_x, cur_y, heights, visited);
+        if cur_x == end_x && cur_y == end_y {
+            return (true, cur_length);
+        }
+        visited[cur_y][cur_x] = true;
+        let cur_height = heights[cur_y][cur_x];
+        for (d_x, d_y) in directions {
+            let new_x = (cur_x as isize + d_x) as usize;
+            let new_y = (cur_y as isize + d_y) as usize;
+            if visited[new_y][new_x] {
+                continue;
+            }
+            if heights[new_y][new_x] <= cur_height + 1 {
+                queue.push_back((new_x, new_y, cur_length + 1));
+            }
+        }
+    }
+    return (false, 0);
+}
+
+#[cfg(debug_assertions)]
+fn print_maps(cur_x: usize, cur_y: usize, heights: &Vec<Vec<u32>>, visited: &Vec<Vec<bool>>) {
+    let start_offset = 'a' as u32 - 1;
+    for y in 0..heights.len() {
+        for x in 0..heights[y].len() {
+            let cur_symbol = char::from_u32(heights[y][x] + start_offset)
+                .unwrap()
+                .to_string();
+            if y == cur_y && x == cur_x {
+                print!("{}", cur_symbol.red());
+            } else if visited[y][x] {
+                print!("{}", cur_symbol.blue());
+            } else {
+                print!("{}", cur_symbol);
+            }
+        }
+        println!("");
+    }
+}
+
+#[cfg(not(debug_assertions))]
+fn print_maps(cur_x: usize, cur_y: usize, heights: &Vec<Vec<u32>>, visited: &Vec<Vec<bool>>) {
+    return;
+}
