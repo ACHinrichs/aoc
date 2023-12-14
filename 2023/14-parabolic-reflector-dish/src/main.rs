d4044782563b6eedb00f8f0a9c38d7a823d62fbc
Adrian Hinrichs <adrian.hinrichs@rwth-aachen.de>
2023-12-14 10:51:48 +0100

    Add non-working sol. for p2

--- 2023/14-parabolic-reflector-dish/src/main.rs
+++ 2023/14-parabolic-reflector-dish/src/main.rs
@@ -1,11 +1,31 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
 fn perform_spin_cycle(inp: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let res = inp.clone();
-    return res;
+    let mut lines = inp.clone();
+
+    for (dir_y, dir_x) in [(-1, 0), (0, -1), (1, 0), (0, 1)] {
+        let mut changes = true;
+        while changes {
+            changes = false;
+            for y in 0..lines.len() {
+                for x in 0..lines[y].len() {
+                    if lines[y][x] == 'O'
+                        && lines[(y as i32 + dir_y) as usize][(x as i32 + dir_x) as usize] == '.'
+                    {
+                        // move rock up
+                        lines[y][x] = '.';
+                        lines[(y as i32 + dir_y) as usize][(x as i32 + dir_x) as usize] = 'O';
+                        changes = true;
+                    }
+                }
+            }
+        }
+    }
+    return lines;
 }
 
 fn main() {
@@ -49,22 +69,51 @@ fn main() {
                 }
             }
         }
-
-        // calculate load
-        let mut load = 0;
-        for y in 0..lines.len() {
-            print!("\n");
-            for x in 0..lines[y].len() {
-                print!("{}", lines[y][x]);
-                if lines[y][x] == 'O' {
-                    load += lines.len() - y - 1;
-                }
-            }
-        }
-
-        println!("The load is {}", load);
     } else if task == "2" {
+        let num_cycles = 1000000000;
+        let mut prev_states = HashMap::new();
+        let mut i = 0;
+        while i < num_cycles {
+            if i % 10000 == 0 {
+                println!("{} / {}", i, num_cycles);
+            }
+            lines = perform_spin_cycle(lines);
+
+            if !prev_states.contains_key(&lines) {
+                prev_states.insert(lines.clone(), i);
+            } else if i * 2 - prev_states.get(&lines).unwrap() < num_cycles {
+                println!(
+                    "Found configuration in step {} last seen {}, next seen {} ?",
+                    i,
+                    prev_states.get(&lines).unwrap(),
+                    i + (i - prev_states.get(&lines).unwrap())
+                );
+                i = i + (i - prev_states.get(&lines).unwrap()); // skip as many of those cycles as possible
+
+                //continue;
+
+                //i = i
+                //    + ((num_cycles - i) / (i - prev_states.get(&lines).unwrap()))
+                //        * (num_cycles - i) as u64;
+                println!("{}", i);
+            }
+            i = i + 1;
+        }
+        println!("{}", i);
     } else {
         panic!("Task unknown, please specify as first argument")
     }
+    // calculate load
+    let mut load = 0;
+    for y in 0..lines.len() {
+        print!("\n");
+        for x in 0..lines[y].len() {
+            print!("{}", lines[y][x]);
+            if lines[y][x] == 'O' {
+                load += lines.len() - y - 1;
+            }
+        }
+    }
+
+    println!("The load is {}", load);
 }
